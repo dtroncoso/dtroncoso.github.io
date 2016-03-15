418fed77588ff4286e562d62f05f7822e5c55a94
dtroncoso <dtroncoso@localhost>
2016-03-15 14:19:30 -0300

    6to commit

---
+++ juegoUno.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var num=1234;
+function verifica()
+{
+    
+}
+
+
